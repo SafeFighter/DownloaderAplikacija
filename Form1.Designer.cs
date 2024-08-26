7735de0c278e5842b7f6a9abb9390bdab9977eee
matija <matoma002@gmail.com>
2024-08-26 13:31:11 +0200

    final

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.unosLinka = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // unosLinka
+            // 
+            this.unosLinka.Location = new System.Drawing.Point(112, 35);
+            this.unosLinka.Name = "unosLinka";
+            this.unosLinka.Size = new System.Drawing.Size(377, 22);
+            this.unosLinka.TabIndex = 0;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(174, 77);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(109, 45);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Paste your link:";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(523, 175);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.unosLinka);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox unosLinka;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+
 
 namespace DownloaderAplikacija
 {
@@ -15,6 +19,60 @@ namespace DownloaderAplikacija
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public async Task DownloadFileAsync(string url, string filePath)
+        {
+            using (HttpClient klijent = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await klijent.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    byte[] fileBytes = await response.Content.ReadAsByteArrayAsync();
+                    
+
+
+                    // Zapisivanje bajtova u datoteku asinhrono
+                    using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None, bufferSize: 4096, useAsync: true))
+                    {
+                        await fileStream.WriteAsync(fileBytes, 0, fileBytes.Length);
+                       
+                    }
+                    MessageBox.Show("Download completed");
+                }
+                catch (Exception ex) {                   
+                    MessageBox.Show("Download failed "+ex);
+                }
+
+            }
+        }
+
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files | *.txt| jpeg | *.jpeg| png| *.png| mp4| *.mp4| mp3| *.mp3";
+            saveFileDialog.Title = "Save an Image File";
+
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                string url = unosLinka.Text;
+
+                await DownloadFileAsync(url, filePath);                        
+            }
+
         }
     }
 }
